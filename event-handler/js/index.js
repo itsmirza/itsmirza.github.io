66241fce015685b60cf6fb6a91918d994b36ee00
Hamizah Hasim <hamizahmathasim@gmail.com>
2021-04-01 21:59:15 +0800

    2195

---
+++ event-handler/js/index.js
@@ -0,0 +1,8 @@
+function transformUppercase(_val){
+    return _val.toUpperCase();
+}
+
+let elNamaPengguna = document.getElementById("namaPengguna"); // 1) Select element
+elNamaPengguna.addEventListener("keyup", function(){
+    elNamaPengguna = transformUppercase(elNamaPengguna.value);
+});
