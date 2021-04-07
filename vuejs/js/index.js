71f9ee465319691b0f6b20f661114761d54dcf2d
Hamizah Hasim <hamizahmathasim@gmail.com>
2021-04-07 20:57:37 +0800

    2057

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -1,11 +1,11 @@
-let app = new Vue({
-    el: "#app",
+let app1 = new Vue({
+    el: "#app1",
     data: {
         message:"Hello Vue"
     }
 });
 
-let app = new Vue({
+let app2 = new Vue({
     el: "#app2",
     data: {
         message: "Loaded on " + new Date().toLocaleString(),
@@ -15,14 +15,14 @@ let app = new Vue({
 });
 
 //if true it will show, if false it will hide
-let app = new Vue({
+let app3 = new Vue({
     el: "#app3",
     data: {
         showIt: true
     }
 });
 
-let app = new Vue({
+let app4 = new Vue({
     el: "#app4",
     data: {
         theList: [
@@ -36,5 +36,17 @@ let app = new Vue({
                 text:"Learn Javascript"
             }
         ]
+    }
+});
+
+let app5 = new Vue({
+    el: "app5",
+    data:{
+        msg: "Hakim Arif"
+    },
+    method: {
+        reveseMsg: function(){
+            this.msg = this.msg.split("").reverse().join("");
+        }
     }
 });
