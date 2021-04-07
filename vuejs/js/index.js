b0710b4cc72f9ae621cd1a2bbe734207b4000343
Hamizah Hasim <hamizahmathasim@gmail.com>
2021-04-07 20:34:52 +0800

    2034

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -1,6 +1,13 @@
-let app = new VTTCue({
+let app = new Vue({
     el: "#app",
     data: {
         message:"Hello Vue"
+    }
+});
+
+let app2 = new Vue ({
+    el: "#app2",
+    data: {
+        message:"Loaded on " + new Date().toLocaleString()
     }
 });
