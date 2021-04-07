5a3b39515e2b575dd343657307846852a99cddad
Hamizah Hasim <hamizahmathasim@gmail.com>
2021-04-07 20:53:33 +0800

    2053

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -5,7 +5,7 @@ let app = new Vue({
     }
 });
 
-let app2 = new Vue({
+let app = new Vue({
     el: "#app2",
     data: {
         message: "Loaded on " + new Date().toLocaleString(),
@@ -15,14 +15,14 @@ let app2 = new Vue({
 });
 
 //if true it will show, if false it will hide
-let app3 = new Vue({
+let app = new Vue({
     el: "#app3",
     data: {
         showIt: true
     }
 });
 
-let app4 = new Vue({
+let app = new Vue({
     el: "#app4",
     data: {
         theList: [
