9161c461dc6bff040ecf66f6e5a891b1b0421004
Hamizah Hasim <hamizahmathasim@gmail.com>
2021-04-14 21:41:41 +0800

    2241

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -10,6 +10,7 @@ function GetBooking(){
     .then(json => {
 
         let bookingNameList = document.getElementById("bookingNameList");
+        let bookingIds = [];
 
         //clear the table rows 
         for(let k = bookingNameList.rows.length-1; k > 0; k-- ){
@@ -22,6 +23,7 @@ function GetBooking(){
             let gPax = json.bookingitems[i].pax;
             let gRemarks =json.bookingitems[i].remarks;
             let gId = json.bookingitems[i].id;
+            let btnId = "delete" + gId;
 
             let row = bookingNameList.insertRow(bookingNameList.rows.length);
             row.insertCell(0).innerHTML =gId;
@@ -30,6 +32,28 @@ function GetBooking(){
             row.insertCell(3).innerHTML =gPax;
             row.insertCell(4).innerHTML =gRemarks;
             row.insertCell(5).innerHTML ="<button id=''" + gId + " class='btn btn-danger'>Delete</button>";
+
+            bookingIds.push(btnId);
+        }
+
+        for (let j = 0; j<bookingIds.length; j++){
+            let el = document.getElementById(bookingIds[j]);
+            el.addEventListener("click", function(){
+                let theId = bookingIds[j].replace("delete", "");
+                DeleteBooking(theId);
+            });
         }
     });
+
+}
+
+function DeleteBooking(id){
+    let url = 'https://api.sheety.co/5cf7d326628fb1e6b1944ef9f71fa9eb/tableapp/bookingitems' + id;
+    fetch(url, {
+        method: 'DELETE',
+    })
+        .then(()=>{
+            alert("Record id" + id + "delete!");
+            GetBooking();
+        });
 }
