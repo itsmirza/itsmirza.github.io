d9edc8ec0c925155057c5e520133059bb767f618
Hamizah Hasim <hamizahmathasim@gmail.com>
2021-04-18 13:18:10 +0800

    1318

---
+++ Project/js/index.js
@@ -0,0 +1,35 @@
+let commentBtn = document.getElementById("commBtn");
+commentBtn.addEventListener("click", function(){
+    let userNameVal = document.getElementById("userName").value;
+    let userEmailVal = document.getElementById("userEmail").value;
+    let userSubVal = document.getElementById("userSubject").value;
+    let userDetailVal = document.getElementById("userDetails").value;
+
+    UserComment(userNameVal, userEmailVal, userSubVal, userDetailVal);
+});
+
+function UserComment(userNameVal, userEmailVal, userSubVal, userDetailVal){
+    let url = 'https://api.sheety.co/5cf7d326628fb1e6b1944ef9f71fa9eb/yuyuchoc/userComment';
+    
+    let body = {
+        userComment: {
+            name : userNameVal,
+            email : userEmailVal,
+            subject : userSubject,
+            details : userDetails
+        }
+    }
+    
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            "content-Type" : "application/json"
+        }
+    })
+    .then((response) => response.json())
+    .then(json => {
+        alert("Thank you "+json.userComment.name+" , for contact us.")
+    });
+
+}
