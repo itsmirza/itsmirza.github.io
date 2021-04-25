c34622d97f7e6b49c8484616316b92f56f4b7450
Hamizah Hasim <hamizahmathasim@gmail.com>
2021-04-25 14:29:38 +0800

    update list

--- Project/js/userDetail.js
+++ Project/js/userDetail.js
@@ -3,3 +3,30 @@ refreshNowBtn.addEventListener("click", function(){
     alert("Get User");
     //GetUserDetails();
 });
+
+function GetUserDetails(){
+
+    let url = 'https://api.sheety.co/5cf7d326628fb1e6b1944ef9f71fa9eb/yuyuchoc/userComment';
+    fetch(url)
+    .then((response) => response.json())
+    .then(json => {
+
+        let userDetails = document.getElementById("userDetails"),
+
+        for(i = 0 ; i <json.userComments.length; i++){
+            let uName = json.userComments[i].name;
+            let uSubject = json.userComments[i].subject;
+            let uDetails = json.userComments[i].details;
+            let uId = json.userComments[i].id;
+
+            let row = userDetails.insertRow(userDetails.rows.length);
+            row.insertCell(0).innerHTML = uId;
+            row.insertCell(1).innerHTML = uName;
+            row.insertCell(2).innerHTML = uSubject;
+            row.insertCell(3).innerHTML = uDetails;
+            row.insertCell(4).innerHTML = "TBC";
+
+        }
+        
+    });
+}
